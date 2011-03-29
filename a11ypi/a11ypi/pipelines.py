cdb5db967548e0a12b4ac54baf59deb2073e0618
Rohit <rohit@StationEros.(none)>
2011-03-29 22:33:28 +0530

    Fixing the Issues

--- a11ypi/a11ypi/pipelines.py
+++ a11ypi/a11ypi/pipelines.py
@@ -6,20 +6,27 @@ import json
 
 class AYpiPipeline(object):
     def __init__(self):
-	self.file = open("a11ypi_dict.json","ab")
+	self.file = open("a11ypi_dict.json","ab+")
+	
 	
    # this method is called to process an item after it has been scraped.
+    
+
     def process_item(self, item, spider):
-	d = {}
-	i=0
+	d = {}	
+	 
+	i = 0
 	# Here we are iterating over the scraped items and creating a dictionary of dictionaries.
 	try:
-	    while i<=len(item["foruri"]):
-		d.setdefault(item["foruri"][i],{}).setdefault(item["rec"][i],{})[item["foruri_id"][i]] = item['thisurl'] + ":" + item["thisid"][i]
+	    while i<len(item["foruri"]):
+		d.setdefault(item["foruri"][i],{}).setdefault(item["rec"][i],{})[item["foruri_id"][i]] = item['thisurl'] + ":" +item["thisid"][i]
 		i+=1
 	except IndexError:
 	    print "Index out of range"
-	# Writing it to a file
+	
 	json.dump(d,self.file)
-	self.file.close()
+
+				
 	return item
+	
+	
--- a11ypi/a11ypi/spiders/AYpi_spider.py
+++ a11ypi/a11ypi/spiders/AYpi_spider.py
@@ -8,14 +8,18 @@ class AYpiSpider(CrawlSpider):
     allowed_domains = ["a11y.in"]
     
     # This is the list of seed URLs to begin crawling with.
-    start_urls = ["http://a11y.in/a11ypi/idea/a11y_firesafety.html", "http://localhost/a11y/renarr.html"]
+    start_urls = ["http://rookie-trekker.blogspot.com"]
     
     # This is the callback method, which is used for scraping specific data
     def parse(self,response):
+	temp = []
 	hxs = HtmlXPathSelector(response)
 	item = AYpiItem()
-	wholeforuri = hxs.select("//@foruri").extract()              # XPath to extract the foruri, which contains both the URL and id in foruri
-	item["foruri"] = [i.split(":")[-2] for i in wholeforuri]     # This contains the URL in foruri
+	wholeforuri = hxs.select("//@foruri").extract()            # XPath to extract the foruri, which contains both the URL and id in foruri
+	for i in wholeforuri:
+		temp.append(i.rpartition(":"))
+	
+	item["foruri"] = [i[0] for i in temp]     # This contains the URL in foruri
 	item["foruri_id"] = [i.split(":")[-1] for i in wholeforuri]  # This contains the id in foruri
 	item['thisurl'] = response.url                                  
 	item["thisid"] = hxs.select("//@foruri/../@id").extract()
